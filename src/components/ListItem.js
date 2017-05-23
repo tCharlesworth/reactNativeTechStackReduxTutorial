b664db0bf250b20503a63baa83334c68f3163343
Tim Charlesworth <tim_charlesworth@outlook.com>
2017-05-23 14:35:19 -0600

    Show library description on touching of library name

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
-import { Text }     from 'react-native';
+import { Text, View, TouchableWithoutFeedback } from 'react-native';
 import { CardItem } from './common';
 
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
 class ListItem extends Component {
+    renderDescription() {
+        const { library, expanded } = this.props;
+        if (expanded) {
+            return (
+                <CardItem>
+                    <Text style={{flex: 1}}>{library.description}</Text>
+                </CardItem>
+            );
+        }
+    }
     render() {
         const { titleStyle } = styles;
+        const { id, title } = this.props.library;
 
         return (
-            <CardItem>
-                <Text style={titleStyle}>{this.props.library.title}</Text>
-            </CardItem>
+            <TouchableWithoutFeedback onPress={() => this.props.selectLibrary(id)}>
+                <View>
+                    <CardItem>
+                        <Text style={titleStyle}>{title}</Text>
+                    </CardItem>
+                    {this.renderDescription()}
+                </View>
+            </TouchableWithoutFeedback>
         );
     }
 }
@@ -21,4 +40,10 @@ const styles = {
     }
 }
 
-export default ListItem;
+const mapStateToProps = (state, ownProps) => {
+    const expanded = state.selectedLibraryId === ownProps.library.id;
+    return { expanded };
+}
+
+
+export default connect(mapStateToProps, actions)(ListItem);
--- src/reducers/SelectionReducer.js
+++ src/reducers/SelectionReducer.js
@@ -1,6 +1,9 @@
 export default (state = null, action) => {
+    console.log('reducing: ', action.payload);
     switch(action.type) {
         case 'SELECT_LIBRARY':
             return action.payload;
+        default:
+            return state;
     }
 }
