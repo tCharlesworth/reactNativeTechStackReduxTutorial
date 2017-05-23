5ace80fe43cf828d4cc558dcf69acf389bffc73c
Tim Charlesworth <tim_charlesworth@outlook.com>
2017-05-23 13:22:48 -0600

    Setup ListView logic for showing the libraries.

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { ListView } from 'react-native';
 
 import { connect } from 'react-redux';
 
 class LibraryList extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+    componentWillMount() {
+        const ds = new ListView.DataSource({ 
+            rowHasChanged: (r1, r2) => r1 != r2
+         });
+
+         this.dataSource = ds.cloneWithRows(this.props.libraries);
+    }
+    renderRow() {
+    }
     render() {
         return (
-            <View>
-            </View>
+            <ListView
+                dataSource={this.dataSource}
+                renderRow={this.renderRow} />
         );
     }
 }
